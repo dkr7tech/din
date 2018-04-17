b28f55e36f1af039c1a93b40d03feba98f37b758
Dinkar Sharma <Dinkar.Sharma@infor.com>
2018-04-17 11:17:04 +0530

    update to spring 5

--- din-models/src/main/java/com/model/user/User.java
+++ din-models/src/main/java/com/model/user/User.java
@@ -15,7 +15,6 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.NamedQuery;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
